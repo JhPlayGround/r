69485cbaed23d7084a8a9a1d21a6c0a3897402a8
juho1210 <wngh9512@gmail.com>
2019-08-26 15:03:18 +0900

    질적 피처를 모델에 넣어본다

---
+++ qualitative feature.R
@@ -0,0 +1,10 @@
+install.packages("ISLR")
+library(ISLR)
+
+data("Carseats")
+str(Carseats)
+
+sales.fit = lm(Sales ~ Advertising + ShelveLoc, data =Carseats)
+summary(sales.fit)
+
+contrasts(Carseats$ShelveLoc)
